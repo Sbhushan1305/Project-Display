384df4888948b7d1aa02b234680eb5521faf59ee
Shashi Bhushan <sbhushan1305@gmail.com>
2023-07-02 11:49:31 +0530

    My movie app v1

--- MyMovie Plan/MyMoviePlan FrontEnd-Angular/MyMoviePlan-Angular/src/app/Pages/movie-control/movie-control.component.ts
+++ MyMovie Plan/MyMoviePlan FrontEnd-Angular/MyMoviePlan-Angular/src/app/Pages/movie-control/movie-control.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MovieClass } from 'src/app/Classes/movie-class';
+import { MoviewServiceService } from 'src/app/Services/moview-service.service';
 
 @Component({
   selector: 'app-movie-control',
@@ -6,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./movie-control.component.css']
 })
 export class MovieControlComponent {
+  movieclass :MovieClass=new MovieClass();
+  constructor(private movieservice: MoviewServiceService, private router: Router) { 
+    
+  }
+  ngOnInit(): void {
+    
+  }
+  saveMovie() {
+    this.movieservice.addMovie(this.movieclass).subscribe(data => {
+      console.log(data);
+      alert("Movie Save Successfully")
+      this.goToHomePage();
+    },
+      error => {console.log(error),alert("OOPS ..Movie Failed to save !")}
+    );
+  }
+  onsubmitAddMovieSubmit(){
+    console.log(this.movieclass);
+    this.saveMovie();
+  }
 
+  goToHomePage() {
+    this.router.navigate(['/home']);
+  }
 }
